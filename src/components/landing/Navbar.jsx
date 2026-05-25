b334007305ae96bb29ead2ca141a6bec510cc3ff
Puneet Gopinath <76863199+PuneetGopinath@users.noreply.github.com>
2026-05-25 15:15:55 +0530

    feat: add logo and cta button in navbar

--- src/components/landing/Navbar.jsx
+++ src/components/landing/Navbar.jsx
@@ -4,8 +4,16 @@
  * License: MIT (see LICENSE)
  */
 
+import { Link } from "react-router";
+
 export default function Navbar() {
     return (
-        <></>
+        <nav className="fixed z-50 text-white top-0 left-0 w-full backdrop-blur-xl bg-zinc-950/70 border-b border-white/10 h-20">
+            <div className="max-w-7xl mx-auto w-full flex items-center justify-between h-full px-6">
+                <h1 className="font-semibold tracking-tight text-xl">Memory <span className="text-purple-500">Garden</span></h1>
+
+                <Link to="/auth/signin" className="rounded-xl px-4 py-2 bg-white text-black hover:scale-105 transition-all duration-300">Get Started</Link>
+            </div>
+        </nav>
     );
 };
