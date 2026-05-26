bc62bea45123efa6be0fcf4fdb8ebe8d410aded5
Puneet Gopinath <76863199+PuneetGopinath@users.noreply.github.com>
2026-05-26 21:40:21 +0530

    feat: add links to different sections of the page in the navbar

--- src/components/landing/Navbar.jsx
+++ src/components/landing/Navbar.jsx
@@ -12,6 +12,12 @@ export default function Navbar() {
             <div className="max-w-7xl mx-auto w-full flex items-center justify-between h-full px-6">
                 <h1 className="font-semibold tracking-tight text-xl">Memory <span className="text-purple-500">Garden</span></h1>
 
+                <div className="gap-8 hidden md:flex">
+                    <a href="#features" className="font-medium text-zinc-400 hover:text-white transition-colors">Features</a>
+                    <a href="#timeline" className="font-medium text-zinc-400 hover:text-white transition-colors">Timeline</a>
+                    <a href="#about" className="font-medium text-zinc-400 hover:text-white transition-colors">About</a>
+                </div>
+
                 <Link to="/auth/signin" className="rounded-xl px-4 py-2 bg-white text-black hover:scale-105 transition-all duration-300">Get Started</Link>
             </div>
         </nav>
