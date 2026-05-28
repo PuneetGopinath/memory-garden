783619dc6c01c3a4b5897991ff6373a23da52552
Puneet Gopinath <76863199+PuneetGopinath@users.noreply.github.com>
2026-05-28 14:58:10 +0530

    feat: add cta buttons and image for hero section

--- src/components/landing/Hero.jsx
+++ src/components/landing/Hero.jsx
@@ -4,16 +4,23 @@
  * License: MIT (see LICENSE)
  */
 
+import { Link } from "react-router";
+
 import Navbar from "./Navbar";
 
 export default function Hero() {
     return (
         <header className="hero bg-zinc-950 text-white">
             <Navbar />
-            <div className="min-h-[calc(100vh-5rem)] grid lg:grid-cols-2 gap-16 px-8 py-16 lg:py-0 max-w-7xl mx-auto w-full items-center pt-20">
+            <div className="min-h-screen grid lg:grid-cols-2 gap-16 px-8 py-16 lg:py-0 max-w-7xl mx-auto w-full items-center pt-20">
                 <div className="flex flex-col space-y-8 max-w-2xl">
                     <h1 className="text-5xl lg:text-7xl font-bold mb-4 leading-tight tracking-tight">Your memories deserve more than folders.</h1>
                     <p className="text-zinc-400 text-lg mb-6 max-w-xl">Memory Garden transforms your precious moments into a living timeline powered by AI.</p>
+                
+                    <div className="flex flex-col sm:flex-row gap-4">
+                        <Link to="/auth/signup" className="font-medium bg-white text-black px-6 py-3 rounded-xl hover:scale-105 transition-all">Start your Garden</Link>
+                        <a href="#timeline" className="font-medium border border-zinc-700 px-6 py-3 rounded-xl hover:bg-white/5 transition-all duration-300">View Demo</a>
+                    </div>
                 </div>
 
                 <div className="flex items-center justify-center relative">
@@ -21,7 +28,7 @@ export default function Hero() {
                     <div className="absolute -bottom-12 right-1/4 h-80 w-80 rounded-full bg-cyan-500/30 opacity-30 blur-3xl"></div>
                 
                     <div className="relative overflow-hidden rotate-1 hover:rotate-0 hover:scale-[1.02] transition-transform duration-300 h-96 w-[340px] p-6 rounded-3xl bg-zinc-900/70 border border-white/10 backdrop-blur-xl shadow-2xl">
-                        <img src="/demo.png" alt="Demo" className="h-36 w-full bg-gradient-to-br from-purple-500 to-cyan-500 object-cover rounded-2xl" />
+                        <img src="../assets/kerala.png" alt="Kerala" className="hover:scale-103 transition-transform duration-300 h-44 w-full bg-gradient-to-br from-purple-500 to-cyan-500 object-cover rounded-2xl" />
                         <div className="space-y-2">
                             <p className="text-sm text-zinc-400 mt-4">July 18, 2025</p>
                             <h5 className="text-xl font-semibold">Trip to Kerala</h5>
