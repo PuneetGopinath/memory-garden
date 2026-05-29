0f74c26707cae36f39ec27892d9c469e476ad6fc
Puneet Gopinath <76863199+PuneetGopinath@users.noreply.github.com>
2026-05-29 14:57:47 +0530

    feat: add supabase client in utility folder

---
+++ src/utils/supabase.js
@@ -0,0 +1,12 @@
+/**
+ * MemoryGarden
+ * Copyright (c) 2026 Puneet Gopinath
+ * License: MIT (see LICENSE)
+ */
+
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
