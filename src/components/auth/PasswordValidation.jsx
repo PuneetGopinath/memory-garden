39169b166d9a858b5757d767510b34c2aeeabf80
Puneet Gopinath <76863199+PuneetGopinath@users.noreply.github.com>
2026-05-31 18:08:43 +0530

    feat: split requirements in password validation, better readability for users

--- src/components/auth/PasswordValidation.jsx
+++ src/components/auth/PasswordValidation.jsx
@@ -11,13 +11,19 @@ const colors = {
 
 export default function PasswordValidation({ pwd }) {
     const eight = pwd.length >= 8;
-    const chars = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]).+$/.test(pwd);
+    const hasUpper = /[A-Z]/.test(pwd);
+    const hasLower = /[a-z]/.test(pwd);
+    const hasNumber = /[0-9]/.test(pwd);
+    const hasSpecial = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(pwd);
 
     return (
         <div className="text-xs p-2">
             <ul className="list-disc list-inside">
-                <li className={`${eight ? colors.right : colors.wrong}`}>Must contain at least 8 characters</li>
-                <li className={`${chars ? colors.right : colors.wrong}`}>Must contain at least one uppercase and one lowercase character, one digit and one special character</li>
+                <li className={`${eight ? colors.right : colors.wrong}`}>At least 8 characters</li>
+                <li className={`${hasUpper ? colors.right : colors.wrong}`}>Uppercase letter</li>
+                <li className={`${hasLower ? colors.right : colors.wrong}`}>Lowercase letter</li>
+                <li className={`${hasNumber ? colors.right : colors.wrong}`}>Number</li>
+                <li className={`${hasSpecial ? colors.right : colors.wrong}`}>Special character</li>
             </ul>
         </div>
     );
