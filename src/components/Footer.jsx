1abdac1aacc014e364b70a3bc59235f7f56f6faa
Puneet Gopinath <76863199+PuneetGopinath@users.noreply.github.com>
2026-06-01 18:29:20 +0530

    feat: make footer reusable for add more links only for landing page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,11 +4,18 @@
  * License: MIT (see LICENSE)
  */
 
-export default function Footer() {
+export default function Footer({ children }) {
     return (
         <footer className="border-t border-white/10 flex flex-col items-center bg-zinc-950 text-white text-center py-4">
-            <span className="text-md font-medium text-zinc-100 mb-2">&copy; 2026 Puneet Gopinath. All rights reserved.</span>
-            <span className="text-sm text-zinc-400">Free during beta • Built for meaningful memories</span>
+            {children}
+
+            <span className="text-md font-medium text-zinc-100 mb-2">
+                &copy; 2026 Puneet Gopinath. All rights reserved.
+            </span>
+
+            <span className="text-sm text-zinc-400">
+                Free during beta • Built for meaningful memories
+            </span>
         </footer>
     );
 };
