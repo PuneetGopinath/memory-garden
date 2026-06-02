1d97e26af7de24eeebf3e920f79b36a250d21d81
Puneet Gopinath <76863199+PuneetGopinath@users.noreply.github.com>
2026-06-02 19:05:43 +0530

    fix: update navigation in ProtectedRoute to share location on unauthenticated access and remove search param
    this is because even on sign out from dashboard we get redirected to signin with that param

--- src/layouts/ProtectedRoute.jsx
+++ src/layouts/ProtectedRoute.jsx
@@ -5,18 +5,19 @@
  */
 
 import { useContext } from "react";
-import { Outlet, Navigate } from "react-router";
+import { Outlet, Navigate, useLocation } from "react-router";
 
 import { AuthContext } from "../context/AuthContext";
 
 export default function ProtectedRoute() {
     const { state } = useContext(AuthContext);
+    const location = useLocation();
 
     if (state === "loading")
         return <div className="text-center">Loading...</div>;
 
     if (state === "unauthenticated")
-        return <Navigate to="/auth/signin?notify=protected" replace />;
+        return <Navigate to="/auth/signin" state={{ from: location.pathname }} replace />;
 
     return <Outlet />;
 };
