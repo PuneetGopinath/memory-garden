b94fdc150e1dff89161fa1bea7e3642193e81c2e
Puneet Gopinath <76863199+PuneetGopinath@users.noreply.github.com>
2026-06-08 22:37:26 +0530

    feat: create a signed url for the memory images from supabase storage

--- src/pages/dashboard/Home.jsx
+++ src/pages/dashboard/Home.jsx
@@ -5,8 +5,11 @@
  */
 
 import { useContext, useState, useEffect } from "react";
+import { Link } from "react-router";
 
 import { AuthContext } from "../../context/AuthContext";
+
+import Timeline from "../../components/Timeline";
 
 import supabase from "../../utils/supabase";
 
@@ -36,24 +39,35 @@ export default function Home() {
                 console.error("[DASHBOARD] Error fetching memories:", error);
                 return;
             }
+            for (const i in data) {
+                if (data[i].image_url)
+                    data[i].image_url = await supabase.storage.from("memory_images").createSignedUrl(data[i].image_url, 60 * 60).signedUrl;
+            }
             setMemories(data);
         };
 
         fetchMemories();
-    }, [user]);
+    }, [user.id]);
 
     return (
         loading
             ? <span className="text-zinc-400">Loading...</span>
             : (
                 <div className="text-center text-white">
-                    <h1 className="text-4xl font-bold mb-4">Welcome, {user.user_metadata.username}!</h1>
+                    <div className="max-w-2xl mx-auto">
+                        <h1 className="text-4xl font-bold mb-4">Hi there, {user.user_metadata.username}!</h1>
+                        <span className="text-zinc-400 font-light text-lg mx-auto p-4">Welcome to your Memory Garden! Plant your memories and let the timeline grow.</span>
+                    </div>
 
-                    <button className="rounded-full border border-white/10 bg-cyan-400 p-2 mt-6 hover:bg-cyan-500">Upload Memories</button>
+                    <Link to="upload" className="rounded-full text-sm font-medium border border-white/10 bg-cyan-400 p-4 mt-6 hover:bg-cyan-500 transition-colors duration-300 inline-block mb-8">
+                        Upload Memories
+                    </Link>
 
-                    {memories.length > 0
-                        ? (<></>)
-                        : (<span className="text-zinc-400 mt-4 block">No memories uploaded yet. Please upload some memories to get started.</span>)}
+                    {
+                        memories.length > 0
+                            ? (<Timeline memories={memories} className="mb-8" />)
+                            : (<span className="text-zinc-400 mt-4 block mb-8">Your memory garden awaits for your first memory. Plant your memories and watch your timeline grow.</span>)
+                    }
                 </div>
             )
     );
