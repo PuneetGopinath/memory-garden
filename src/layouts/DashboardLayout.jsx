3715fd5693ad7dc35a97b2b359da25a14f051178
Puneet Gopinath <76863199+PuneetGopinath@users.noreply.github.com>
2026-06-10 15:27:44 +0530

    feat: enhance layout of dashboard with sign-out button and header
    header contains links to various pages and sign out button

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -8,14 +8,39 @@ import { Outlet } from "react-router";
 
 import Footer from "../components/Footer";
 
+import supabase from "../utils/supabase";
+
 export default function DashboardLayout() {
+    const handleSignOut = async () => {
+        await supabase.auth.signOut();
+    };
+    
     return (
         <>
-            <main>
-                <Outlet />
-            </main>
+            <header className="bg-zinc-950">
+                <nav className="fixed top-0 left-0 right-0 bg-zinc-950/80 backdrop-blur border-b border-white/10 text-white p-4 flex justify-end gap-4 z-10">
+                    <div className="absolute left-4 top-1/2 -translate-y-1/2">
+                        <h2 className="text-lg font-bold text-white">Memory <span className="text-purple-500">Garden</span></h2>
+                    </div>
 
-            <Footer />
+                    <div className="flex items-center gap-6">
+                        <a href="#" className="font-medium text-zinc-400 hover:text-white transition-colors">Dashboard</a>
+                        <a href="#" className="font-medium text-zinc-400 hover:text-white transition-colors">Timeline</a>
+                        <a href="#" className="font-medium text-zinc-400 hover:text-white transition-colors">Profile</a>
+                        <button onClick={handleSignOut} className="font-medium text-zinc-400 hover:text-white transition-colors">
+                            Sign Out
+                        </button>
+                    </div>
+                </nav>
+            </header>
+
+            <div className="min-h-screen">
+                <main className="bg-zinc-950 text-white pt-20 pb-4">
+                    <Outlet />
+                </main>
+
+                <Footer />
+            </div>
         </>
     );
 };
