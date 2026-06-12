4cbf8281eca21efbdca106afd9c74436e124fbe7
Puneet Gopinath <76863199+PuneetGopinath@users.noreply.github.com>
2026-06-12 12:18:31 +0530

    feat: add styles and subtitle for edit memory page

--- src/pages/dashboard/Edit.jsx
+++ src/pages/dashboard/Edit.jsx
@@ -69,48 +69,53 @@ export default function Edit() {
     return loading
         ? <span>Loading...</span>
         : (
-            <div>
-                <div>
-                    <h1>Edit Memory</h1>
+            <div className="text-white bg-zinc-900 p-6 rounded-lg border border-white/10 backdrop-blur max-w-lg mx-auto mb-12">
+                <div className="mb-6 text-center space-y-2">
+                    <h1 className="font-semibold text-4xl">Edit Memory</h1>
+                    <span className="text-zinc-400">Make your memory plants error free</span>
                 </div>
 
                 <form onSubmit={handleEdit} className="space-y-4">
-                    <label>
-                        <span>Title <span className="text-red-500">*</span></span>
+                    <label className="flex flex-col gap-1">
+                        <span className="text-sm font-medium">Title <span className="text-red-500">*</span></span>
                         <input
                             type="text"
+                            className="rounded-lg bg-zinc-800/50 w-full border border-white/10 px-4 py-2 focus:outline-none focus:border-cyan-600/70"
                             value={title}
                             onChange={(e) => setTitle(e.target.value)}
                         />
                     </label>
 
-                    <label>
-                        <span>Description</span>
+                    <label className="flex flex-col gap-1">
+                        <span className="text-sm font-medium">Description</span>
                         <textarea
                             value={desc}
+                            className="rounded-lg bg-zinc-800/50 w-full border border-white/10 px-4 py-2 focus:outline-none focus:border-cyan-600/70"
                             onChange={(e) => setDesc(e.target.value)}
                         />
                     </label>
 
-                    <label>
-                        <span>Date <span className="text-red-500">*</span></span>
+                    <label className="flex flex-col gap-1">
+                        <span className="text-sm font-medium">Date <span className="text-red-500">*</span></span>
                         <input
                             type="date"
+                            className="rounded-lg bg-zinc-800/50 w-full border border-white/10 px-4 py-2 focus:outline-none focus:border-cyan-600/70"
                             value={date}
                             onChange={(e) => setDate(e.target.value)}
                         />
                     </label>
 
-                    <label>
-                        <span>Image</span>
+                    <label className="flex flex-col gap-1">
+                        <span className="text-sm font-medium">Image</span>
                         <input
                             type="file"
+                            className="rounded-lg bg-zinc-800/50 w-full border border-white/10 px-4 py-2 focus:outline-none focus:border-cyan-600/70"
                         />
                     </label>
 
                     <button
                         type="submit"
-                        className="rounded-full font-bold bg-cyan-500 hover:bg-cyan-400 disabled:bg-cyan-300 disabled:hover:bg-cyan-300 disabled:cursor-not-allowed"
+                        className="rounded-full mt-4 p-2 font-bold bg-cyan-500 hover:bg-cyan-400 disabled:bg-cyan-300 disabled:hover:bg-cyan-300 disabled:cursor-not-allowed"
                         disabled={loading || noChange}
                     >
                         {loading && !noChange ? "Saving Changes..." : "Save Changes"}
