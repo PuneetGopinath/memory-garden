aab3557c668b0442478548d600dc18f907f3358c
Puneet Gopinath <76863199+PuneetGopinath@users.noreply.github.com>
2026-06-12 17:55:34 +0530

    fix: correctly handle loading state and properly create signed url for images

--- src/pages/dashboard/Home.jsx
+++ src/pages/dashboard/Home.jsx
@@ -24,25 +24,40 @@ export default function Home() {
         const fetchMemories = async () => {
             let data, error;
             try {
-            ({ data, error } = await supabase
-                .from("memories")
-                .select("*")
-                .eq("user_id", user.id));
+                ({ data, error } = await supabase
+                    .from("memories")
+                    .select("*")
+                    .eq("user_id", user.id));
             } catch (err) {
                 console.error("[DASHBOARD] Unexpected error fetching memories:", err);
-                return;
-            } finally {
                 setLoading(false);
+                return;
             }
             
             if (error) {
                 console.error("[DASHBOARD] Error fetching memories:", error);
+                setLoading(false);
                 return;
             }
-            for (const i in data) {
-                if (data[i].image_url)
-                    data[i].image_url = await supabase.storage.from("memory_images").createSignedUrl(data[i].image_url, 60 * 60).signedUrl;
+
+            try {
+                for (let i = 0; i < data.length; i++) {
+                    data[i].date = new Date(data[i].memory_date).toLocaleDateString("en-US", { year: "numeric", month: "long", day: "numeric" });
+                    if (data[i].image_path) {
+                        const { data: d, error: imgErr } = await supabase.storage.from("memory_images").createSignedUrl(data[i].image_path, 60 * 60);
+                        if (imgErr)
+                            throw imgErr;
+                        
+                        const { signedUrl } = d;
+                        data[i].img = signedUrl;
+                    }
+                }
+            } catch (err) {
+                console.error("[DASHBOARD] Error fetching memory images:", err);
+            } finally {
+                setLoading(false);
             }
+
             setMemories(data);
         };
 
@@ -53,7 +68,7 @@ export default function Home() {
         loading
             ? <span className="text-zinc-400">Loading...</span>
             : (
-                <div className="text-center text-white">
+                <div className="text-white text-center">
                     <div className="max-w-2xl mx-auto">
                         <h1 className="text-4xl font-bold mb-4">Hi there, {user.user_metadata.username}!</h1>
                         <span className="text-zinc-400 font-light text-lg mx-auto p-4">Welcome to your Memory Garden! Plant your memories and let the timeline grow.</span>
@@ -63,11 +78,13 @@ export default function Home() {
                         Upload Memories
                     </Link>
 
-                    {
-                        memories.length > 0
-                            ? (<Timeline memories={memories} className="mb-8" />)
-                            : (<span className="text-zinc-400 mt-4 block mb-8">Your memory garden awaits for your first memory. Plant your memories and watch your timeline grow.</span>)
-                    }
+                    <div className="text-left">
+                        {
+                            memories.length > 0
+                                ? (<Timeline memories={memories} className="mb-8" links={true} />)
+                                : (<span className="text-zinc-400 mt-4 block mb-8">Your memory garden awaits for your first memory. Plant your memories and watch your timeline grow.</span>)
+                        }
+                    </div>
                 </div>
             )
     );
