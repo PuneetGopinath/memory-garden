a0c212f037ecbb811b0f7c906d43fbf73f77722f
Puneet Gopinath <76863199+PuneetGopinath@users.noreply.github.com>
2026-06-12 18:45:47 +0530

    feat: set min 20rem width and conditionally render the image otherwise render gradient

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -9,12 +9,14 @@ import { Link } from "react-router";
 function TimelineEvent({ date, title, description, img, clockwise, mood, link }) {
     return (
         <Link to={link} className="block">
-            <div className={`relative overflow-hidden ${clockwise ? "rotate-1" : "-rotate-1"} hover:rotate-0 hover:scale-[1.02] transition-transform duration-300 min-h-[24rem] w-full max-w-sm p-6 rounded-3xl bg-zinc-900/70 border border-white/10 backdrop-blur-xl shadow-2xl`}>
-                <img src={img} alt={title} className="hover:scale-103 transition-transform duration-300 h-44 w-full bg-gradient-to-br from-purple-500 to-cyan-500 object-cover rounded-2xl" />
+            <div className={`relative overflow-hidden ${clockwise ? "rotate-1" : "-rotate-1"} hover:rotate-0 hover:scale-[1.02] transition-transform duration-300 min-h-[24rem] min-w-[20rem] w-full max-w-sm p-6 rounded-3xl bg-zinc-900/70 border border-white/10 backdrop-blur-xl shadow-2xl`}>
+                {img
+                    ? <img src={img} alt={title} className="h-44 w-full object-cover rounded-2xl hover:scale-103 transition-transform duration-300" />
+                    : <div className="w-full h-44 rounded-2xl bg-gradient-to-br from-purple-500 to-cyan-500 hover:scale-103 transition-transform duration-300"></div>}
                 <div className="space-y-3 mt-4">
                     <p className="text-sm text-zinc-400">{date}</p>
                     <h5 className="text-xl font-semibold">{title}</h5>
-                    <p className="text-zinc-400 leading-relaxed">{description}</p>
+                    {description && <p className="text-zinc-400 leading-relaxed">{description}</p>}
                     {mood && <span className="inline-flex hover:translate-y-[-2px] transition-transform duration-300 items-center rounded-full px-3 py-1 text-xs font-medium uppercase tracking-wide bg-purple-500/10 border border-purple-500/20 text-purple-300">{mood}</span>}
                 </div>
             </div>
