bbb1fae51ac361886101d1fe4ba83971eb1bc068
Puneet Gopinath <76863199+PuneetGopinath@users.noreply.github.com>
2026-06-26 20:17:11 +0530

    feat: implement same loading screen in protected route

--- src/layouts/ProtectedRoute.jsx
+++ src/layouts/ProtectedRoute.jsx
@@ -14,7 +14,19 @@ export default function ProtectedRoute() {
     const location = useLocation();
 
     if (state === "loading")
-        return <div className="min-h-screen bg-zinc-950 text-white text-center">Loading...</div>;
+        return (
+            <div className="min-h-screen bg-zinc-950 text-white flex items-center justify-center">
+                <div className="flex flex-col text-center items-center gap-6 px-8" role="status" aria-live="polite" aria-label="Checking authentication status">
+                    <div className="w-25 h-25 border-4 border-purple-500 border-dashed rounded-full animate-[spin_4s_linear_infinite]"></div>
+                    <h1 className="text-2xl font-bold">Checking your session...</h1>
+                    <p className="text-sm text-zinc-400">
+                        Please wait while we verify your authentication status.
+                        <br />
+                        This may take a few seconds.
+                    </p>
+                </div>
+            </div>
+        );
 
     if (state === "unauthenticated")
         return <Navigate to="/auth/signin" state={{ from: location.pathname }} replace />;
--- src/layouts/PublicOnlyRoute.jsx
+++ src/layouts/PublicOnlyRoute.jsx
@@ -12,7 +12,7 @@ import { AuthContext } from "../context/AuthContext";
 export default function PublicOnlyRoute() {
     const { state } = useContext(AuthContext);
 
-    if (state === "loading") {
+    if (state === "loading")
         return (
             <div className="bg-zinc-950 min-h-screen text-white flex items-center justify-center">
                 <div className="flex flex-col text-center items-center gap-6 px-8" role="status" aria-live="polite" aria-label="Checking authentication status">
@@ -26,7 +26,6 @@ export default function PublicOnlyRoute() {
                 </div>
             </div>
         );
-    }
 
     if (state === "authenticated")
         return <Navigate to="/dashboard?notify=signedin" replace />;
